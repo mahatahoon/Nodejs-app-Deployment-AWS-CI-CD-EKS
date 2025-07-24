9b22ebecbace8db78ff65a15de29dbd5ee8e844c
MahaTahoon <tahoonmaha@gmail.com>
2025-07-24 18:17:11 +0300

    Update app.js

--- app.js
+++ app.js
@@ -3,15 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/api', (req, res) => {
-  res.send(`
-    <html>
-      <head>
-        <title>Hello Eyego</title>
-      </head>
-      <body style="display: flex; justify-content: center; align-items: center; height: 100vh; background-color: #f9f9f9;">
-        <h1 style="font-size: 3em; color: #2c3e50;">👋 Hello, Eyego!</h1>
-      </body>
-    </html>
+  res.send(`Hello EYEGO! Welcome to my app.\n
   `);
 });
 
