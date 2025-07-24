6e6cdbde05526fbacf785d8b8c9710f3f679b03f
Maha Tahoon <tahoonmaha@gmail.com>
2025-07-24 17:17:19 +0300

    added app code files

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get('/hello', (req, res) => {
+app.get('/api', (req, res) => {
   res.send('Hello, World! Welcome to iQuant YouTube Channel.\n');
 });
 
